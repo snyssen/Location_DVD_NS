c8548c9673312a33a13534d7c01e3e6fec019066
Simon NYSSEN <Simon.NYSSEN@outlook.be>
2018-06-09 11:31:48 +0200

    + Vérification au lancement que le programme n'est pas déjà ouvert

--- Location_DVD_NS/Program.cs
+++ Location_DVD_NS/Program.cs
@@ -14,9 +14,24 @@ namespace Location_DVD_NS
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EcranAccueil());
+            if (ExisteInstance())
+                MessageBox.Show("Une autre instance du programme est déjà active !");
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new EcranAccueil());
+            }
+        }
+        static bool ExisteInstance()
+        {
+            System.Diagnostics.Process actu = System.Diagnostics.Process.GetCurrentProcess();
+            System.Diagnostics.Process[] acti = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process p in acti)
+                if (p.Id != actu.Id)
+                    if (actu.ProcessName == p.ProcessName)
+                        return true;
+            return false;
         }
     }
 }
